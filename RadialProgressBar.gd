bbe40ba8fca9bd250eadb25d8099e45772a33674
vovinhd <timo.volk@gmail.com>
2020-10-16 17:53:22 +0200

    gotta commit fast

--- RadialProgressBar.gd
+++ RadialProgressBar.gd
@@ -55,6 +55,6 @@ func draw_circle_arc(center, radius, angle_from, angle_to, color):
 	points_arc.push_back(center)
 	var colors = PoolColorArray([color])
 	for i in range(nb_points + 1):
-	    var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
-	    points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+		var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
+		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
 	draw_polygon(points_arc, colors)
